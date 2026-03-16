675d0e7d002b755af9bce13e7566b939f661bc30
Hemp04 <hbparikh4@gmail.com>
2026-03-16 23:00:36 +0530

    Added tiptap extension and its components

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { TaskItem, TaskList } from "@tiptap/extension-list";
+import {
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "@tiptap/extension-table";
+import { ResizableImage } from "tiptap-extension-resizable-image";
+import Image from "@tiptap/extension-image";
+
+export const Editor = () => {
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        style: "padding-left:56px; padding-right:56px;",
+        class:
+          "focus:outline-none print:border-0 bg-white border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      },
+    },
+
+    extensions: [
+      StarterKit,
+      Table,
+      TableHeader,
+      TableCell,
+      TableRow,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+      Image.configure({
+        inline: false,
+        allowBase64: true,
+        HTMLAttributes: {
+          class: "resizable-image",
+        },
+      }),
+      //   ResizableImage,
+    ],
+
+    content: `
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th colspan="3">Description</th>
+          </tr>
+          <tr>
+            <td>Cyndi Lauper</td>
+            <td>Singer</td>
+            <td>Songwriter</td>
+            <td>Actress</td>
+          </tr>
+        </tbody>
+      </table>
+    `,
+
+    immediatelyRender: false,
+  });
+
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white">
+      <div className="min-w-max flex justify-center w-204 py-4 mx-auto">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
---
+++ src/app/documents/page.tsx
@@ -0,0 +1,10 @@
+const DocumentsPage = () => {
+  return (
+    <div>
+      <h1>Documents</h1>
+      <p>Welcome to the Documents page!</p>
+    </div>
+  );
+};
+
+export default DocumentsPage;
